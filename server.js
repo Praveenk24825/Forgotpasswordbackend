03232d0b9fc9fce1300f79038447227687f2de12
Praveenk24825 <praveenk24825@gmail.com>
2025-09-08 15:37:48 +0530

    cvb

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
-// ✅ Proper CORS setup
-app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:5173"], // allow frontend
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -24,5 +18,5 @@ app.get("/", (req, res) => res.send("🚀 API running"));
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
